19fd9ace37d59c915bf052cd8583c9df6b22c0a1
DuetoPark <inhye.park.dev@gmail.com>
2024-07-13 18:49:00 +0900

    ✨ 로컬 웹 서버에 연결

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import queryString from "query-string";
+import { useLocation } from "react-router-dom";
+import io from "socket.io-client";
 
 import "./Chat.css";
 
+let socket;
+const END_POINT = "http://localhost:8080";
+
 const Chat = () => {
+  const { search } = useLocation();
+
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(search);
+
+    socket = io(END_POINT);
+
+    setName(name);
+    setRoom(room);
+
+    console.log(socket);
+
+    return () => {};
+  }, [search]);
+
   return <div>Chat</div>;
 };
 
