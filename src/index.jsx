8c24601eaac38f725b10e2884409056d9d17600f
DuetoPark <inhye.park.dev@gmail.com>
2024-07-13 17:45:03 +0900

    🎨 route 설정

---
+++ src/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import App from "./App";
+import ErrorPage from "./pages/error-page/ErrorPage";
+import Join from "./components/Join/Join";
+import Chat from "./components/Chat/Chat";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        index: true,
+        element: <Join />,
+      },
+      {
+        path: "/chat",
+        element: <Chat />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
