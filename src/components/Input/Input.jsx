dadf2fff2b05890fa15ff71ae78e7e5ee0ae3226
DuetoPark <inhye.park.dev@gmail.com>
2024-07-14 00:24:26 +0900

    ♻️ sendMessage를 form의 submit 이벤트로 수정

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,24 +3,21 @@ import React from "react";
 import "./Input.css";
 
 const Input = ({ message, setMessage, sendMessage }) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    sendMessage(event);
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleSubmit}>
       <input
         type="text"
         className="input"
         placeholder="Type a message..."
         value={message}
         onChange={(event) => setMessage(event.currentTarget.value)}
-        onKeyUp={(event) => {
-          if (event.key === "Enter") {
-            sendMessage(event);
-          }
-        }}
       />
-      <button
-        className="sendButton"
-        onClick={(event) => event.preventDefault()}
-      >
+      <button type="submit" className="sendButton">
         Send
       </button>
     </form>
