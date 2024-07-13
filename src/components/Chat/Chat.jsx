e83b49141f784df9fa3d53b0d824cd0dc50bbdd5
DuetoPark <inhye.park.dev@gmail.com>
2024-07-13 23:47:08 +0900

    ✨ 메시지 출력 기능 구현

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -5,6 +5,7 @@ import io from "socket.io-client";
 
 import InfoBar from "../InfoBar/InfoBar";
 import Input from "../Input/Input";
+import Messages from "../Messages/Messages";
 
 import "./Chat.css";
 
@@ -57,6 +58,8 @@ const Chat = () => {
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room} />
+
+        <Messages messages={messages} name={name} />
 
         <Input
           message={message}
