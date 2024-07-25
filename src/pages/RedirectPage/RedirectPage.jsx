e055235ac0c42b700e731bae6c67880383156c16
DuetoPark <inhye.park.dev@gmail.com>
2024-07-25 21:56:25 +0900

    🩹 console.log 제거, error는 console.error로 대체

--- src/pages/RedirectPage/RedirectPage.jsx
+++ src/pages/RedirectPage/RedirectPage.jsx
@@ -6,24 +6,22 @@ const RedirectPage = ({ children }) => {
   const { search } = useLocation();
   const { name, room } = queryString.parse(search);
 
-  console.log(queryString.parse(search));
-
   const noQueryString = !name || !room;
 
   // NOTE: queryString이 없는 경우, 홈으로 redirect
   if (noQueryString) {
-    console.log("No query string, redirecting to /");
+    console.error("No query string, redirecting to /");
     return <Navigate to="/" replace />;
   }
 
   // NOTE: room이 air인 경우, AirConditioner로 redirect
   if (room === "air") {
-    console.log("Room is air, redirecting to /air");
+    console.error("Room is air, redirecting to /air");
     return <Navigate to={`/air?name=${name}&room=null`} />;
   }
 
   // NOTE: room이 air가 아닌 경우, Chat으로 이동
-  console.log("Room is not air, rendering children");
+  console.error("Room is not air, rendering children");
   return children;
 };
 
